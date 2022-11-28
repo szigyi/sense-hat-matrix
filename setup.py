8d4891b2beb5410b2be972c7defe25e5ff9b507f
Sabi <szigyi.szogyi@gmail.com>
2022-11-28 20:26:30 +0000

    using solution to make pypi friendly README

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@
 
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 if __name__ == "__main__":
-    setup()
+    setup(long_description=long_description, long_description_content_type='text/markdown')
